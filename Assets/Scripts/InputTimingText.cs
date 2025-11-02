f89669649a262bd11081586a525239cacb2d6f61
Anthony Pagone <anthony21@pagone.com>
2025-11-01 23:45:11 -0700

    Add simple judgement calculation and display input judgement onscreen

---
+++ Assets/Scripts/InputTimingText.cs
@@ -0,0 +1,60 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InputTimingText : MonoBehaviour
+{
+
+    /// <summary>
+    /// The text component that belongs to the "InputTimingText" node in the editor.
+    /// </summary>
+    public Text text;
+    /// <summary>
+    /// The default string that another string can be added onto later.
+    /// </summary>
+    private string defaultText = "Hit Spacebar to the beat.\nYour hit time is:\n";
+    /// <summary>
+    /// The conductor node in the editor tree.
+    /// </summary>
+    public Conductor conductor;
+
+    /// <summary>
+    /// Start is called once before the first execution of Update after the MonoBehaviour is created
+    /// </summary>
+    void Start()
+    {
+        text.text = defaultText;
+    }
+
+    /// <summary>
+    /// Update is called once per frame
+    /// </summary>
+    void Update()
+    {
+        // TODO: Allow the user to change controls
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+
+            double hitTime = GetHitTime();
+            Judgement judgement = Judgements.GetRating(hitTime);
+            text.text = defaultText + hitTime + " ms. " + judgement;
+        }
+    }
+
+    /// <summary>
+    /// Return the hit time in milliseconds.
+    /// </summary>
+    /// <returns></returns>
+    double GetHitTime()
+    {
+        // Modular division by quarterNote because there is a beat every quarterNote seconds.
+        double hitTime = conductor.songPosition % conductor.quarterNote;
+        // If the hit time is greater than half the time between beats, the hit is counted for the next beat instead.
+        if (hitTime > conductor.quarterNote / 2)
+        {
+            hitTime -= conductor.quarterNote;
+        }
+        // Then convert seconds to milliseconds.
+        hitTime *= 1000.0;
+        return hitTime;
+    }
+}
---
+++ Assets/Scripts/Judgements.cs
@@ -0,0 +1,41 @@
+using System;
+
+/// <summary>
+/// An enum representing the different possible judgements.
+/// </summary>
+public enum Judgement
+{
+    Miss,
+    Good,
+    Perfect
+}
+    
+class Judgements
+{
+    // Timings were decided by Kai.
+    /// Time required for a good in milliseconds
+    public const int goodTime = 80;
+    /// Time required for a perfect in milliseconds
+    public const int perfectTime = 40;
+
+    /// <summary>
+    /// Gets the rating for a hit time.
+    /// The hit time must be in milliseconds.
+    /// </summary>
+    /// <param name="hitTime"></param>
+    /// <returns></returns>
+    public static Judgement GetRating(double hitTime)
+    {
+        // If hit time is within perfect range
+        if (hitTime > -perfectTime && hitTime < perfectTime)
+        {
+            return Judgement.Perfect;
+        }
+        // If hit time is within good range
+        if (hitTime > -goodTime && hitTime < goodTime)
+        {
+            return Judgement.Good;
+        }
+        return Judgement.Miss;
+    }
+}
