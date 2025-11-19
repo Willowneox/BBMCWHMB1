d4482c7d6cf6a2c3756c74254c4c0f86246669b7
Anthony Pagone <anthony21@pagone.com>
2025-11-18 20:02:27 -0800

    Shorten scene name

--- Assets/Scripts/Main_Menu/SceneSwitcher.cs
+++ Assets/Scripts/Main_Menu/SceneSwitcher.cs
@@ -5,6 +5,6 @@ public class SceneSwitcher : MonoBehaviour
 {
     public void load_level_3()
     {
-        SceneManager.LoadScene("Assets/Scenes/Delivery_Level.unity");
+        SceneManager.LoadScene("Delivery_Level");
     }
 }
