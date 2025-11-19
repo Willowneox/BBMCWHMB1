6c39be23d91e02718b3fae46133fd1618969a8e4
the-elig <elidg@uci.edu>
2025-11-19 13:52:46 -0800

    completed pause menu except for settings

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -1,9 +1,43 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
 
 public class UIManager : MonoBehaviour
 {
+    [SerializeField] private GameObject _inGameText;
+    [SerializeField] private GameObject _pauseMenu;
+
     public void OnRestartPress()
     {
+        AudioListener.pause = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
 
+    public void OnPausePress()
+    {
+        AudioListener.pause = true;
+
+        _inGameText.SetActive(false);
+        _pauseMenu.SetActive(true);
+    }
+
+    public void OnHomePress()
+    {
+        SceneManager.LoadScene("main menu");
+    }
+
+    public void OnContinuePress()
+    {
+        _inGameText.SetActive(true);
+        _pauseMenu.SetActive(false);
+
+        AudioListener.pause = false;
+    }
+
+    public void OnLevelThreePress()
+    {
+        SceneManager.LoadScene("Delivery_Level");
+        AudioListener.pause = false;
     }
 }
